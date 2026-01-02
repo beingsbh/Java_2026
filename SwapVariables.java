654455e026271cda79e5897c029f4244df0c4bc6
Subhankar Das <beingsbh07@gmail.com>
2026-01-03 00:26:30 +0530

    day2 2nd

---
+++ SwapVariables.java
@@ -0,0 +1,18 @@
+
+class SwapVariables {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int x =0;
+System.out.println("A = "+a+"B = "+b);
+x=a;
+a=b;
+b=x;
+
+
+System.out.println(a+"  "+b);
+    }
+
+
+}
