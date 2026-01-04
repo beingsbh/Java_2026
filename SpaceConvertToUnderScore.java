44ee04d7638a764e8b53d84c0ccd19fd1d94dce8
Subhankar Das <beingsbh07@gmail.com>
2026-01-04 17:32:18 +0530

    day 4 commit

---
+++ SpaceConvertToUnderScore.java
@@ -0,0 +1,8 @@
+public class SpaceConvertToUnderScore {
+    public static void main(String[] args) {
+        String name = "To The Principel";
+        String text = name.replace(" ", "_");
+        System.out.println(text);
+    }
+    
+}
