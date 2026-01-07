8e7829458cdd0b55e3a3331b7004addc1a9da9a9
Subhankar Das <beingsbh07@gmail.com>
2026-01-07 22:18:50 +0530

    day 7 program 1

---
+++ Multi_D_Array.java
@@ -0,0 +1,15 @@
+public class Multi_D_Array {
+public static void main(String[] args) {
+    int [][] m_Array = {{1,2,3},{4,5,6}};
+
+
+  for(int [] row : m_Array){
+    for(int value : row){
+        System.out.print(value+ " ");
+    }
+    System.out.println();
+  }
+       
+}
+    
+}
