b5acb0621a828edd859bb30d6e28ee342592efe0
Subhankar Das <beingsbh07@gmail.com>
2026-01-07 22:52:22 +0530

    day 7 program 2

---
+++ Add_array_elements.java
@@ -0,0 +1,12 @@
+public class Add_array_elements {
+    public static void main(String[] args) {
+float [] eleMents = {1.2f,2.45f,3.34f,4.4f,5.44f};
+float add = 0.0f;
+for(int i = 0 ; i<eleMents.length;i++){
+ add = add + eleMents[i];
+    
+}
+System.out.println(add);
+    }
+    
+}
