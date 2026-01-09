0671c4c494e0df1205bfec9579e3d3a9859e87ea
Subhankar Das <beingsbh07@gmail.com>
2026-01-09 22:14:25 +0530

    day 9

--- VarArgs.java
+++ VarArgs.java
@@ -3,14 +3,10 @@ public class VarArgs {
         int r = 0;
         for (int a : arr){
             r += a;
-
         }
         return r;
     }
     public static void main(String[] args) {
-        
-    
-
       int b=  Sum(5+4);
         System.out.println(b);
 
