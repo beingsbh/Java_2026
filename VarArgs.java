1af9ccf28681601bd84a35e6fb9a23bd7483cd9b
Subhankar Das <beingsbh07@gmail.com>
2026-01-09 12:05:49 +0530

    remove duplicate

---
+++ VarArgs.java
@@ -0,0 +1,16 @@
+public class VarArgs {
+    static int Sum (int ...arr){
+        int r = 0;
+        for (int a : arr){
+            r += a;
+
+        }
+        return r;
+    }
+    static void main(String [] args) {
+
+      int b=  Sum(5+4);
+        System.out.println(b);
+
+    }
+}
