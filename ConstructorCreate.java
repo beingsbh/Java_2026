5c6c7c0175b38586e607fa400b99c92ebd2a7cf2
Subhankar Das <beingsbh07@gmail.com>
2026-01-12 18:37:57 +0530

    Day 12

---
+++ ConstructorCreate.java
@@ -0,0 +1,17 @@
+public class ConstructorCreate {
+
+     ConstructorCreate() {
+    System.out.println("Constructor Caalled");
+    
+    }
+    
+    
+    public static void main(String[] args) {
+
+       ConstructorCreate obj =  new ConstructorCreate ();
+
+      
+        
+    }
+    
+}
