5a666d93af5e97ef7878e6b5219f000859010d96
Subhankar Das <beingsbh07@gmail.com>
2026-01-13 19:33:37 +0530

    update 13.01.2026

--- RockPaSe.java
+++ RockPaSe.java
@@ -1,21 +1,39 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaSe {
+
     public static void main(String[] args) {
         Scanner SC = new Scanner(System.in);
         Random RD = new Random();
-        
-        int c = RD.nextInt((3)+1);
-        System.out.println(" Press '1' = Rock \n Press '2' = Paper \n Press '3' = Seccior ");
-       System.out.print(" Enter Your Turn : ");
-       int u = SC.nextInt();
-      
-  if((u==1 && c==3) || (u==2 && c==1 ) || (u==3 && c==1) ){
-    System.out.println("You Win");
+        int c = RD.nextInt(3) + 1;
+        int n;
+        System.out.println("\nLets Play ! \n Press '1' Forr Rock \n Press '2' For Paper  \n Press '3' For Seccior \n");
+        System.out.println("Enter Your Turn : ");
+        n = SC.nextInt();
+        if (n > 3 || n == 0) {
+            System.out.println("Invalid Input");
+        } else if ((c == 1 && n == 2) || (c == 2 && n == 3) || (c == 3 && n == 1)) {
+            System.out.println("You Won");
+
+        } else if (c == n) {
+            System.out.println("Draw ! Retry");
+        } else {
+            System.out.println("You Loose !");
+        }
+
+        switch (c) {
+            case 1 ->
+                System.out.println("Computer Choose Rock");
+
+            case 2 ->
+                System.out.println("Computer Choose Paper ");
+
+            default ->
+                System.out.println("Computer Choose Seccior");
+        }
+
     }
-    else
-        System.out.println("You Lose");
-    
-}
+
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,21 +1,33 @@
-public class tempCodeRunnerFile {
-int speed;
-String car;
+import java.util.Random;
+import java.util.Scanner;
 
-    public tempCodeRunnerFile(int s ,String c) {
-
-        speed = s;
-        c=car;
-        System.out.println(c+" Top Speed Is "+ s);
-
-    }
-    void display(){
-        
-        System.out.println(car+" Top Speed Is "+ speed);
-    }
+public class RockPaSe{
     public static void main(String[] args) {
-        tempCodeRunnerFile display = new tempCodeRunnerFile(350, "Lamborghini");
-        display.display();
+        Scanner SC = new Scanner(System.in);
+        Random RD = new Random();
+        int c = RD.nextInt(3)+1;
+        int n ;
+        System.out.println("\nLets Play ! \n Press '1' Forr Rock \n Press '2' For Paper  \n Press '3' For Seccior \n");
+    System.out.println("Enter Your Turn : ");
+    n=SC.nextInt();
+    if( n >3 || n==0){
+        System.out.println("Invalid Input");
     }
-    
+   else if ((c==1 && n==2) || (c==2 && n==3) || (c==3 && n==1)) {
+        System.out.println("You Loos !");
+        
+    }
+    else if(c==n){
+        System.out.println("Draw ! Retry");
+    }
+ 
+    else{
+        System.out.println("You Win !");
+    }
+        
+
+
+
+    }
+
 }
